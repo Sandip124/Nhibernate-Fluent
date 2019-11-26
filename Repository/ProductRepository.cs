1c8b17537d11f67227b2ba934d50ae40e7ed9afa
sandip124 <sandip.choudhary@crystalsolution.com.np>
2019-11-26 08:02:05 +0545

    feat: add product repository

---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Nhibernate_Fluent.Base.BaseRepository;
+using Nhibernate_Fluent.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nhibernate_Fluent.Repository
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+       
+    }
+}
