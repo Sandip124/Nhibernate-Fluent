ad93f80dbd0dcdea9eedb8fab1330ad0fe66bc84
sandip124 <sandip.choudhary@crystalsolution.com.np>
2019-11-26 07:59:36 +0545

    feat add base Repository  interface and implementation.

---
+++ Base/BaseRepository/BaseRepository.cs
@@ -0,0 +1,54 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nhibernate_Fluent.Base.BaseRepository
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+
+        public void delete(T entities)
+        {
+            ISession session = SessionFactory.SessionFactory.getCurrentSession();
+            session.Delete(entities);
+
+
+        }
+        public void insert(T entities)
+        {
+
+            ISession session = SessionFactory.SessionFactory.getCurrentSession();
+
+            session.Save(entities);
+
+        }
+        public void update(T entities)
+        {
+
+            ISession session = SessionFactory.SessionFactory.getCurrentSession();
+
+            session.Update(entities);
+
+        }
+        public IList<T> getAll()
+        {
+            IList<T> lists;
+            ISession session = SessionFactory.SessionFactory.getCurrentSession();
+            lists = session.QueryOver<T>().List<T>();
+            return lists;
+        }
+        public IQueryOver<T, T> getQueryable()
+        {
+            ISession session = SessionFactory.SessionFactory.getCurrentSession();
+            return session.QueryOver<T>();
+        }
+
+        public T getById(long id)
+        {
+            ISession session = SessionFactory.SessionFactory.getCurrentSession();
+            return session.Get<T>(id);
+        }
+    }
+}
---
+++ Base/BaseRepository/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nhibernate_Fluent.Base.BaseRepository
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        void delete(T entities);
+        void insert(T entities);
+        void update(T entities);
+        IList<T> getAll();
+        IQueryOver<T, T> getQueryable();
+        T getById(long id);
+    }
+}
