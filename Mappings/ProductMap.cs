4c2b9aae487daa08710ef47b72c77b620b6e72a5
sandip124 <sandip.choudhary@crystalsolution.com.np>
2019-11-26 08:01:28 +0545

    feat: add product mapping

---
+++ Mappings/ProductMap.cs
@@ -0,0 +1,25 @@
+﻿using FluentNHibernate.Mapping;
+using Nhibernate_Fluent.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nhibernate_Fluent.Mappings
+{
+    public class ProductMap : ClassMap<Product>
+    {
+        public ProductMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name);
+            Map(x => x.Price);
+            HasManyToMany(x => x.StoresStockedIn)
+                .Cascade.All()
+                .Inverse()
+                .Table("StoreProduct");
+
+            Component(x => x.Location);
+        }
+    }
+}
