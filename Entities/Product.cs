f3dc75b690aff409c7807a8744903ea86d5e158a
sandip124 <sandip.choudhary@crystalsolution.com.np>
2019-11-26 08:00:48 +0545

    feat: add location Component in the Product entity.

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -10,11 +10,18 @@ namespace Nhibernate_Fluent.Entities
         public virtual int Id { get; protected set; }
         public virtual string Name { get; set; }
         public virtual double Price { get; set; }
+        public virtual Location Location { get; set; }
+
         public virtual IList<Store> StoresStockedIn { get; protected set; }
 
         public Product()
         {
             StoresStockedIn = new List<Store>();
         }
+    }
+    public class Location
+    {
+        public virtual int Aisle { get; set; }
+        public virtual int Shelf { get; set; }
     }
 }
