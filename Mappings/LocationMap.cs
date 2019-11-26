fbd71a1c87bec5a0aef2df5df9f737a59363e6c6
sandip124 <sandip.choudhary@crystalsolution.com.np>
2019-11-26 08:01:14 +0545

    feat: add location Component Mapping

---
+++ Mappings/LocationMap.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+using Nhibernate_Fluent.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nhibernate_Fluent.Mappings
+{
+    public class LocationMap : ComponentMap<Location>
+    {
+        public LocationMap()
+        {
+            Map(x => x.Aisle);
+            Map(x => x.Shelf);
+        }
+    }
+}
